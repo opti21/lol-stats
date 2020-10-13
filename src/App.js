d6491efff8ec70b3425673caf7572dc7ba25c07e
opti21 <browe.media@gmail.com>
2020-10-13 00:49:02 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+import { Line } from "react-chartjs-2";
 
 function App() {
+
+
+  const [stats, setStats] = useState([{ name: 0, value: 0 }])
+  const [seconds, setSeconds] = useState(1)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get("https://timeapi.herokuapp.com/utc/now")
+        .then(res => {
+          console.log(res)
+
+          const newGold = Math.floor(Math.random() * 100)
+
+          setTime(oldTime => [...oldTime, oldTime.length.toString()])
+
+          setStats(oldStats => [
+            ...oldStats,
+            newGold
+          ])
+
+
+          console.log(stats)
+
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LOL Stats</h1>
+      <div>{JSON.stringify(stats)}</div>
+      <Line data={data} />
     </div>
   );
 }
