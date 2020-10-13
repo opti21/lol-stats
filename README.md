4b13e791c7573bf2cad3bc08b9322093a3640df3
opti21 <browe.media@gmail.com>
2020-10-13 10:09:37 -0500

    Added health and level charts

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-> LOL STATS
+LOL STATS
 
 just a quick liitle app to track my LOL stats live
+using the Riot Games Live client data API
+
+still needs to disable CORS to work because I'm lazy lol
--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend  } from 'recharts';
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
 
 function App() {
-  const [goldChartData, setGoldChartData] = useState([]);
-  const [goldText, setGoldText] = useState('0')
+  const [goldData, setGoldData] = useState([]);
+  const [goldText, setGoldText] = useState("0");
+
+  const [healthData, setHealthData] = useState([]);
+  const [healthText, setHealthText] = useState("0");
+
+  const [levelData, setLevelData] = useState([]);
+  const [levelText, setLevelText] = useState("0");
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -14,16 +29,23 @@ function App() {
         .then((res) => {
           console.log(res.data);
 
-          const newTime = Math.floor(res.data.gameData.gameTime).toString()
-          const newGold = res.data.activePlayer.currentGold 
-          const newData = {name: newTime, gold: newGold}
-          
-          setGoldChartData(oldData => [
-            ...oldData,
-            newData
-          ])
+          const newTime = Math.floor(res.data.gameData.gameTime).toString();
+          const newGold = res.data.activePlayer.currentGold;
+          const newLevel = res.data.activePlayer.level;
+          const newHealth = res.data.activePlayer.championStats.currentHealth;
 
-          setGoldText(Math.floor(newGold.toString()))
+          const newGoldData = { name: newTime, gold: newGold};
+          setGoldData(oldData => [...oldData, newGoldData]);
+          setGoldText(Math.floor(newGold.toString()));
+
+          const newLevelData = { name: newTime, level: newLevel};
+          setLevelData(oldData => [...oldData, newLevelData]);
+          setLevelText(Math.floor(newLevel.toString()));
+
+          const newHealthData = { name: newTime, health: newHealth};
+          setHealthData(oldData => [...oldData, newHealthData]);
+          setHealthText(Math.floor(newHealth.toString()));
+
         })
         .catch((err) => {
           console.error(err);
@@ -35,18 +57,53 @@ function App() {
   return (
     <div className="App">
       <h1>LOL Stats</h1>
-    <div>
-      <h3>Current Gold: {goldText}</h3>
-    </div>
-    <LineChart width={730} height={350} data={goldChartData}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5  }}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="gold" stroke="#8884d8" />
-    </LineChart>
+      <div>
+        <h3>Current Gold: {goldText}</h3>
+      </div>
+      <ResponsiveContainer width={"100%"} height={250}>
+        <LineChart
+          data={goldData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="gold" stroke="#F00" />
+        </LineChart>
+      </ResponsiveContainer>
+
+      <div>
+        <h3>Current Health: {healthText}</h3>
+      </div>
+      <ResponsiveContainer width={"100%"} height={250}>
+        <LineChart
+          data={healthData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="health" stroke="#F00" />
+        </LineChart>
+      </ResponsiveContainer>
+
+      <div>
+        <h3>Current Level: {levelText}</h3>
+      </div>
+      <ResponsiveContainer width={"100%"} height={250}>
+        <LineChart
+          data={levelData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="level" stroke="#F00" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
