4b75481dea6f7ee54519ca6a74f25df3f44a086d
opti21 <browe.media@gmail.com>
2020-10-13 04:53:31 -0500

    finally found a chart library to use and it works

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
-import { Line } from "react-chartjs-2";
+import "./App.css";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend  } from 'recharts';
 
 function App() {
-
-
-  const [stats, setStats] = useState([{ name: 0, value: 0 }])
-  const [seconds, setSeconds] = useState(1)
+  const [goldChartData, setGoldChartData] = useState([]);
+  const [goldText, setGoldText] = useState('0')
 
   useEffect(() => {
     const interval = setInterval(() => {
-      axios.get("https://timeapi.herokuapp.com/utc/now")
-        .then(res => {
-          console.log(res)
+      axios
+        .get("https://127.0.0.1:2999/liveclientdata/allgamedata")
+        .then((res) => {
+          console.log(res.data);
 
-          const newGold = Math.floor(Math.random() * 100)
-
-          setTime(oldTime => [...oldTime, oldTime.length.toString()])
-
-          setStats(oldStats => [
-            ...oldStats,
-            newGold
+          const newTime = Math.floor(res.data.gameData.gameTime).toString()
+          const newGold = res.data.activePlayer.currentGold 
+          const newData = {name: newTime, gold: newGold}
+          
+          setGoldChartData(oldData => [
+            ...oldData,
+            newData
           ])
 
-
-          console.log(stats)
-
+          setGoldText(Math.floor(newGold.toString()))
         })
-        .catch(err => {
-          console.error(err)
-        })
+        .catch((err) => {
+          console.error(err);
+        });
     }, 1000);
     return () => clearInterval(interval);
   }, []);
@@ -38,8 +35,18 @@ function App() {
   return (
     <div className="App">
       <h1>LOL Stats</h1>
-      <div>{JSON.stringify(stats)}</div>
-      <Line data={data} />
+    <div>
+      <h3>Current Gold: {goldText}</h3>
+    </div>
+    <LineChart width={730} height={350} data={goldChartData}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5  }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="gold" stroke="#8884d8" />
+    </LineChart>
     </div>
   );
 }
